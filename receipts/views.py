517874f65ffe2a47c14e9900066d9bf8ebe26074
Yazan <yzan.alsalhani@gmail.com>
2024-05-23 19:37:24 +0300

    Feature 5 complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from receipts.models import Receipt
 # Create your views here.
+def home(request):
+    receipts = Receipt.objects.all()
+    context={
+        "receipts": receipts
+    }
+    return render(request, "receipts/home.html", context)
